926b949b9e0dfd9e5f613ee5001775df2d8e7914
Colin Quirk <cquirk@uchicago.edu>
2020-04-27 16:14:53 -0500

    Add colors

--- eyeliner/image.py
+++ eyeliner/image.py
@@ -24,7 +24,22 @@ class Image():
         self.context.rectangle(0, 0, self.screen_width, self.screen_height)
         self.context.fill()
 
-    def draw(self, points, color=None):
+    @staticmethod
+    def _calculate_color(i, length):
+        proportion = i / length
+        color_value = round(proportion * (256 * 2))  # number of possible colors
+
+        divisions = color_value // 256
+        remainder = color_value % 256
+
+        if divisions == 0:
+            color = ((255 - remainder) / 256, remainder / 256, 0)
+        elif divisions == 1:
+            color = (0, (255 - remainder) / 256, remainder / 256)
+
+        return color
+
+    def draw(self, points, color=False):
         try:
             points[0][1]
         except (TypeError, KeyError, IndexError):
@@ -33,14 +48,20 @@ class Image():
         if len(points[0]) != 2:
             raise(TypeError('Length of each point (x,y) must be equal to 2.'))
 
-        self.context.move_to(points[0][0], points[0][1])
-
-        for p in points:
-            self.context.line_to(p[0], p[1])
-
-        self.context.set_source_rgb(0., 0., 0.)
         self.context.set_line_width(10)
-        self.context.stroke()
+        self.context.move_to(points[0][0], points[0][1])
+        self.context.set_source_rgb(0., 0., 0.)
+
+        if color:
+            for i, p in enumerate(points):
+                self.context.set_source_rgb(*self._calculate_color(i, len(points)))
+                self.context.move_to(points[i-1][0], points[i-1][1])
+                self.context.line_to(p[0], p[1])
+                self.context.stroke()
+        else:
+            for p in points:
+                self.context.line_to(p[0], p[1])
+            self.context.stroke()
 
     def write(self, filename):
         self.surface.write_to_png(filename)
--- tests/test_image.py
+++ tests/test_image.py
@@ -17,11 +17,32 @@ class TestImage(unittest.TestCase):
         self.assertEqual(img2.screen_width, 1600)
         self.assertEqual(img2.screen_height, 1200)
 
+    def test_convert_color(self):
+        img = eyeliner.Image()
+        values = [
+            (0.99609375, 0.0, 0.0),
+            (0.796875, 0.19921875, 0.0),
+            (0.59765625, 0.3984375, 0.0),
+            (0.39453125, 0.6015625, 0.0),
+            (0.1953125, 0.80078125, 0.0),
+            (0.0, 0.99609375, 0.0),
+            (0.0, 0.796875, 0.19921875),
+            (0.0, 0.59765625, 0.3984375),
+            (0.0, 0.39453125, 0.6015625),
+            (0.0, 0.1953125, 0.80078125)
+        ]
+
+        for i in range(10):
+            color = img._calculate_color(i, 10)
+            for j, c in enumerate(color):
+                self.assertAlmostEqual(c, values[i][j])
+
     def test_image_draw(self):
         img = eyeliner.Image()
         points = [(960, 540), (20, 20), (20, 1060), (1900, 1060), (1900, 20), (960, 540)]
 
         img.draw(points)
+        img.draw(points, color=True)
 
         with self.assertRaises(TypeError):
             img.draw(100)
@@ -42,6 +63,20 @@ class TestImage(unittest.TestCase):
         img = eyeliner.Image()
         points = [(960, 540), (20, 20), (20, 1060), (1900, 1060), (1900, 20), (960, 540)]
         img.draw(points)
+        img.write(fname)
+
+        self.assertTrue(os.path.exists(fname))
+
+    def test_color_image_output(self):
+        fname = 'test_color.png'
+        try:
+            os.remove(fname)
+        except FileNotFoundError:
+            pass
+
+        img = eyeliner.Image()
+        points = [(960, 540), (20, 20), (20, 1060), (1900, 1060), (1900, 20), (960, 540)]
+        img.draw(points, color=True)
         img.write(fname)
 
         self.assertTrue(os.path.exists(fname))
