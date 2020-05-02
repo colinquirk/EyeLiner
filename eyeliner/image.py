f5348c89bffb9f274c8aed019fd7a6c1c79d4bcb
Colin Quirk <cquirk@uchicago.edu>
2020-05-01 20:54:25 -0500

    switch to matplotlib

--- eyeliner/image.py
+++ eyeliner/image.py
@@ -1,29 +1,20 @@
 """Docstring
 """
 
-import cairo
+import matplotlib.pyplot as plt
 
 
 class Image():
-    def __init__(self, out_size=224, screen_width=1920, screen_height=1080, zoom=1):
-        self.out_size = out_size
+    def __init__(self, screen_width=1920, screen_height=1080, zoom=0):
         self.screen_width = screen_width
         self.screen_height = screen_height
+
+        if zoom <= -0.5 or zoom >= 0.5:
+            raise ValueError('zoom must be greater than -0.5 and less than 0.5')
+
         self.zoom = zoom
 
-        self.surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, out_size, out_size)
-        self.context = cairo.Context(self.surface)
-
-        self._setup_context()
-
-    def _setup_context(self):
-        self.context.scale(self.out_size / self.screen_width,
-                           self.out_size / self.screen_height)
-
-        # white background
-        self.context.set_source_rgb(1., 1., 1.)
-        self.context.rectangle(0, 0, self.screen_width, self.screen_height)
-        self.context.fill()
+        self.fig = None
 
     @staticmethod
     def _calculate_color(i, length):
@@ -41,18 +32,6 @@ class Image():
 
         return color
 
-    def _zoom(self, points):
-        center = (self.screen_width / 2, self.screen_height / 2)
-        print(center)
-        zeroed = [(x - center[0], y - center[1]) for x, y in points]
-        print(zeroed[0])
-        zoomed_zeroed = [(x * self.zoom, y * self.zoom) for x, y in zeroed]
-        print(zoomed_zeroed[0])
-        zoomed = [(x + center[0], y + center[1]) for x, y in zoomed_zeroed]
-        print(zoomed[0])
-
-        return zoomed
-
     def draw(self, points, color=False):
         try:
             points[0][1]
@@ -62,26 +41,18 @@ class Image():
         if len(points[0]) != 2:
             raise(TypeError('Length of each point (x,y) must be equal to 2.'))
 
-        print('draw')
-        print(points[0])
-        points = self._zoom(points)
-        print(points[0])
-        print('done')
+        x_zoom_offset = self.zoom * self.screen_width
+        y_zoom_offset = self.zoom * self.screen_height
 
-        self.context.set_line_width(10)
-        self.context.move_to(points[0][0], points[0][1])
-        self.context.set_source_rgb(0., 0., 0.)
+        self.fig, ax = plt.subplots(figsize=(4, 4), dpi=56)
+        ax.axis('off')
+        ax.set_xlim(x_zoom_offset, self.screen_width - x_zoom_offset)
+        ax.set_ylim(y_zoom_offset, self.screen_height - y_zoom_offset)
 
         if color:
-            for i, p in enumerate(points):
-                self.context.set_source_rgb(*self._calculate_color(i, len(points)))
-                self.context.move_to(points[i-1][0], points[i-1][1])
-                self.context.line_to(p[0], p[1])
-                self.context.stroke()
+            pass
         else:
-            for p in points:
-                self.context.line_to(p[0], p[1])
-            self.context.stroke()
+            ax.plot(*zip(*points), color="black")
 
     def write(self, filename):
-        self.surface.write_to_png(filename)
+        self.fig.savefig(filename)
